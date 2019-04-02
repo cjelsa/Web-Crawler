9ea3fa7d66800f80649c602c977f72bbdb4afc96
mithileshtipkari <mithilesh.tipkari@gmail.com>
2019-04-02 18:01:02 +0530

    Fix

--- webCrawler.py
+++ webCrawler.py
@@ -1,6 +1,6 @@
-
 def fetch_n_crawl_urls():
-    path = 'E:/crawledFiles' #to store downloaded files
+	path = 'E:/crawledFiles' #to store downloaded files
+	print(path)
 
 
 if __name__ == '__main__':
