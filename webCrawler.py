5c8a0fe1be0ff47fa68b28652caab77b263b1986
mithileshtipkari <mithilesh.tipkari@gmail.com>
2019-04-03 01:18:00 +0530

    Added code to read command line arguments

--- webCrawler.py
+++ webCrawler.py
@@ -1,7 +1,14 @@
-def fetch_n_crawl_urls():
-	path = 'E:/crawledFiles' #to store downloaded files
-	print(path)
+import sys
+
+
+def fetch_n_crawl_urls(url, path):
+	print('fetching...')
 
 
 if __name__ == '__main__':
-	fetch_n_crawl_urls()
+    print(sys.argv)
+    url = sys.argv[1]
+    file_path = sys.argv[2]
+    print('Data will be downloaded from-', url)
+    print('Documents will be stored at-', file_path)
+    fetch_n_crawl_urls(url, file_path)
