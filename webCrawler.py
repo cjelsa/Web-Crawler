0296f798d8c7b4158c0ec70b351856b429873d6a
mithileshtipkari <mithilesh.tipkari@gmail.com>
2019-04-03 12:24:22 +0530

    Added argument parser to fetch arguments from command line

--- webCrawler.py
+++ webCrawler.py
@@ -1,14 +1,23 @@
 import sys
-
+import argparse
 
 def fetch_n_crawl_urls(url, path):
 	print('fetching...')
 
 
 if __name__ == '__main__':
-    print(sys.argv)
-    url = sys.argv[1]
-    file_path = sys.argv[2]
-    print('Data will be downloaded from-', url)
-    print('Documents will be stored at-', file_path)
-    fetch_n_crawl_urls(url, file_path)
+	parser = argparse.ArgumentParser()
+	parser.add_argument('url', help='URL from which you want to download files')
+	parser.add_argument('path', help='Path to which you want to dump downloaded files')
+	parser.add_argument('--level', help='Level of recursively visiting pages from a link, default = 3', type=int, default=3)
+	args = parser.parse_args()
+
+	print('url-', args.url)
+	print('path-', args.path)
+	print('level-', args.level)
+	# print(sys.argv)
+	# url = sys.argv[1]
+	# file_path = sys.argv[2]
+	# print('Data will be downloaded from-', url)
+	# print('Documents will be stored at-', file_path)
+	# fetch_n_crawl_urls(url, file_path)
