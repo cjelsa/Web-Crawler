c8318995bbbd85abc2e0c4af7018a08e5ec5f163
mithileshtipkari <mithilesh.tipkari@gmail.com>
2019-04-07 00:24:11 +0530

    Added command line option for filetypes

--- webCrawler.py
+++ webCrawler.py
@@ -10,11 +10,13 @@ if __name__ == '__main__':
 	parser.add_argument('url', help='URL from which you want to download files')
 	parser.add_argument('path', help='Path to which you want to dump downloaded files')
 	parser.add_argument('--level', help='Level of recursively visiting pages from a link, default = 3', type=int, default=3)
+        parser.add_argument('--filetypes', help='Specify comma seperated filetypes such as pdf,txt to be downloaded, default is pdf', default='pdf')
 	args = parser.parse_args()
 
 	print('url-', args.url)
 	print('path-', args.path)
 	print('level-', args.level)
+        print('filetypes-', args.filetypes)
 	# print(sys.argv)
 	# url = sys.argv[1]
 	# file_path = sys.argv[2]
